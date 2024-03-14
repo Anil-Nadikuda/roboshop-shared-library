6a6771bada1474456558d6de0e78b0f3760b87f7
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2024-03-14 13:15:02 +0530

    jenkins multi

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -1,50 +1,52 @@
-pipeline {
-    agent {
-        node {
-            label 'AGENT-1'
-        }
-    }
-    environment {
-        packageVersion = ''
-        nexusURL = '172.31.83.22:8081'
-    }
-    options {
-        timeout(time: 1, unit: 'HOURS')
-        disableConcurrentBuilds()
-        ansiColor('xterm')
-    }
-    parameters {
-    
-
-        booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
-
-        // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
-
-        // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-    }
-    stages {
-        stage('Get the version') {
-            steps {
-                script {
-                    def packageJson = readJSON file: 'package.json'
-                    packageVersion = packageJson.version
-                    echo "application version: $packageVersion"
-                }
+def call(Map configMap){
+    pipeline {
+        agent {
+            node {
+                label 'AGENT-1'
             }
         }
+        environment {
+            packageVersion = ''
+            nexusURL = '172.31.83.22:8081'
+        }
+        options {
+            timeout(time: 1, unit: 'HOURS')
+            disableConcurrentBuilds()
+            ansiColor('xterm')
+        }
+        parameters {
+        
 
-    }
-    // post build
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-            deleteDir()
+            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+
+            // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+
+            // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
         }
-        failure { 
-            echo 'this runs when pipeline is failed, used generally to send some alerts'
+        stages {
+            stage('Get the version') {
+                steps {
+                    script {
+                        def packageJson = readJSON file: 'package.json'
+                        packageVersion = packageJson.version
+                        echo "application version: $packageVersion"
+                    }
+                }
+            }
+
         }
-        success{
-            echo 'I will say Hello when pipeline is success'
+        // post build
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            failure { 
+                echo 'this runs when pipeline is failed, used generally to send some alerts'
+            }
+            success{
+                echo 'I will say Hello when pipeline is success'
+            }
         }
     }
 }
